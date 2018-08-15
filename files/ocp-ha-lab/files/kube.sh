88ff91638eb78bbb4f0de5b62367818ecf507d33
Tiffany Jachja <tjachja@redhat.com>
2018-08-15 15:02:00 -0400

    pv.sh

---
+++ files/ocp-ha-lab/files/kube.sh
@@ -0,0 +1,6 @@
+ansible masters[0] -b -m fetch -a "src=/root/.kube/config dest=/root/.kube/config flat=yes"
+# Create directories on the support1.$GUID.internal NFS server to be used as PVs in the OpenShift cluster. 
+# These directories should be under /srv/nfs because this directory is backed by a separate volume group.
+export GUID=`hostname|awk -F. '{print $2}'`
+echo "export GUID=$GUID" >> ~/.bashrc
+echo $GUID
