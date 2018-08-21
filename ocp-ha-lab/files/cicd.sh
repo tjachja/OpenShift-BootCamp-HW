2eb8245cebc130c19f8b1b9d511fda429f8629a5
Tiffany Jachja <tjachja@redhat.com>
2018-08-21 18:56:45 -0400

    applier cicd

--- ocp-ha-lab/files/cicd.sh
+++ ocp-ha-lab/files/cicd.sh
@@ -1,14 +1,9 @@
 #!/usr/bin/env bash
 oc login -u andrew -p r3dh4t1!
-## 1. Create Lifecycle Stages
-oc create -f ../container-pipelines/tasks/applier/projects/projects.yml
 
-## 2. Stand up Jenkins master in dev
-oc new-app jboss-eap70-openshift:1.6~https://github.com/wkulhanek/openshift-tasks
-oc process openshift//jenkins-persistent | oc apply -f- -n tasks-build
+oc new-project tasks-build --display-name "Tasks App - Build"
+oc new-project tasks-dev --display-name "Tasks App - Dev"
+oc new-project tasks-stage --display-name "Tasks App - Stage"
+oc new-project tasks-prod --display-name "Tasks App - Prod"
 
-## 3. Instantiate Pipeline
-oc process -f ../container-pipelines/tasks/applier/templates/deployment.yml --param-file=../container-pipelines/tasks/applier/params/deployment-dev | oc apply -f-
-oc process -f ../container-pipelines/tasks/applier/templates/deployment.yml --param-file=../container-pipelines/tasks/applier/params/deployment-stage | oc apply -f-
-oc process -f ../container-pipelines/tasks/applier/templates/deployment.yml --param-file=../container-pipelines/tasks/applier/params/deployment-prod | oc apply -f-
-oc process -f ../container-pipelines/tasks/applier/templates/build.yml --param-file ../container-pipelines/tasks/applier/params/build-dev | oc apply -f-
+ansible-playbook -i container-pipelines/tasks/applier/inventory/ openshift-applier/playbooks/openshift-cluster-seed.yml
